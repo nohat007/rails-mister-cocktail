1a88aafc23dfd22b604b7a3884beb124e8f77196
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-02-15 12:14:40 -0500

    now with style

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -22,7 +22,9 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @dose.delete
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+
   end
 
   private
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
-  belongs_to :ingredient, dependent: :destroy
-  belongs_to :cocktail, dependent: :destroy
+  belongs_to :ingredient
+  belongs_to :cocktail
   validates :description, presence: true
   validates :cocktail_id, presence: true
   validates :ingredient_id, presence: true
