f1c6c571ca31c4431edfe8f83182e463f77587af
nohat yacinthe <nohatyacinthe1@hotmail.fr>
2019-02-15 13:41:53 -0500

    close

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,10 @@ Cocktail.destroy_all
 
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "chocolate")
+Ingredient.create(name: "water")
 
-Cocktail.create(name: "one")
-Cocktail.create(name: "two")
-Cocktail.create(name: "three")
+
+# Cocktail.create(name: "Martini", remote_photo_url: "https://www.thespruceeats.com/thmb/pgXW-PU_GqN9PiaMcOdQYCqHFaE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vodka-martini-recipe-760983-Hero-5bd771cd4cedfd0026121758.jpg")
+# Cocktail.create(name: "two" remote_photo_url: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/bloody-mary.jpg?itok=8lQzuHXm")
+# Cocktail.create(name: "three")
